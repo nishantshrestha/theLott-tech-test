f51f5701937dc00e85f88d06da5b403a0ae2b459
Nishant <nishant.shrestha@hotmail.com>
2017-11-14 20:30:19 +0545

    Added CompanyViewModel.

---
+++ theLott/ViewModels/CompanyViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CompanyViewModel.swift
+//  theLott
+//
+//  Created by Nishant Shrestha on 14/11/17.
+//  Copyright © 2017 Nishant Shrestha. All rights reserved.
+//
+
+import Foundation
+
+struct CompanyViewModel {
+    
+    private var company: Company
+    
+    init(company: Company) {
+        self.company = company
+    }
+    
+    var logoURL: URL? {
+        return URL(string: company.logoURL)
+    }
+}
