1255d5add0ff4bd603f184299d50734771a9d8a7
Nishant <nishant.shrestha@hotmail.com>
2017-11-14 19:55:49 +0545

    Added MockCompanyService class.

---
+++ theLottTests/Services/MockCompanyService.swift
@@ -0,0 +1,25 @@
+//
+//  MockCompanyService.swift
+//  theLottTests
+//
+//  Created by Nishant Shrestha on 14/11/17.
+//  Copyright © 2017 Nishant Shrestha. All rights reserved.
+//
+
+import Foundation
+
+struct MockCompanyService: CompanyService {
+    
+    var state: SuccessfulState
+    
+    init(expectedState: SuccessfulState = .some) {
+        self.state = expectedState
+    }
+    
+    func getCompanies(completionHandler: (Result<[Company]>) -> ()) {
+        switch state {
+            case .empty: completionHandler(.success([]))
+            case .some: completionHandler(.success(MockData().getCompanies() ?? []))
+        }
+    }
+}
