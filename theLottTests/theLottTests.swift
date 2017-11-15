8c3c1ede6348608a4c2fd8611074919f5ef93dae
Nishant <nishant.shrestha@hotmail.com>
2017-11-15 07:39:18 +0545

    Added tests for CompanyTableViewModel.

--- theLottTests/theLottTests.swift
+++ theLottTests/theLottTests.swift
@@ -21,4 +21,46 @@ class theLottTests: XCTestCase {
         XCTAssertTrue(companies.count == 5, "Companies count(\(companies.count)) should be equal to 5.")
     }
     
+    func testTableViewModelCorrectlyHandlesEmptyCompaniesArray() {
+        
+        let service = ServiceFactory.mockEmptyCompaniesService
+        let viewModel = CompaniesTableViewModel(service: service)
+        
+        XCTAssertTrue(viewModel.getNumberOfRowsInSection(section: 0) == 0, "Number of rows should be 0.")
+    }
+    
+    func testTableViewModelCorrectlyHandlesNonEmptyCompaniesArray() {
+        
+        let service = ServiceFactory.mockNonEmptyCompaniesService
+        let viewModel = CompaniesTableViewModel(service: service)
+        
+        XCTAssertTrue(viewModel.getNumberOfRowsInSection(section: 0) == 5, "Number of rows should be 5.")
+    }
+    
+    func testTableViewModelCorrectlyHandlesErrorState() {
+        
+        let service = ServiceFactory.mockFailingComapnyService
+        let viewModel = CompaniesTableViewModel(service: service)
+        
+        XCTAssertEqual(viewModel.errorMessage, "Something went wrong while fetching companies.", "View model should have the specified error message.")
+        XCTAssertTrue(viewModel.getNumberOfRowsInSection(section: 0) == 0, "Number of rows should be 0.")
+    }
+    
+    func testTableViewModelCorrectlyMapsCompaniesToCompaniesViewModel() {
+        
+        let service = ServiceFactory.mockNonEmptyCompaniesService
+        let viewModel = CompaniesTableViewModel(service: service)
+        
+        if let firstCompanyViewModel = viewModel.companyViewModels.first {
+            XCTAssertEqual(firstCompanyViewModel.description, "QLD Residents", "First company view model description does not match.")
+            
+            guard let url = firstCompanyViewModel.logoURL else {
+                XCTFail("First company view model logoURL cannot be nil.")
+                return
+            }
+            
+            XCTAssertEqual(url, URL(string: "http://tim.media.tatts.com/TattsServices/Lotto/Companies/GoldenCasket_v1.png")!, "First company view model URL is incorrect")
+        }
+    }
+    
 }
