3bdac03c395d24f7873aeb84dc33246cb2d2e1c7
Nishant <nishant.shrestha@hotmail.com>
2017-11-15 18:39:10 +0545

    Added clarification for comments.

--- theLottTests/Models/ExpectedState.swift
+++ theLottTests/Models/ExpectedState.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// To be passed as a dependency to MockCompanyService that will determine what sort of network response it will return
 enum ExpectedState {
     case some
     case empty
